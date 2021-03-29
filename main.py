40471e810b850978451b3f7330e409a9e875c4e4
Nik Steinbruegge <ni851ste@htwg-konstanz.de>
2021-03-29 11:57:21 +0100

    Refactored Code mostly into classes

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import random
 window_width = "1000"
 window_height = "600"
 
-
-drawingSpace2=0
+# TODO -1- Needs work for clean separation into classes
+drawingCanvas = None
 
 
 def drag_start(event):
@@ -21,47 +21,7 @@ def drag_motion(event):
     widget.place(x=x, y=y)
 
 
-def init_window():
-    global drawingSpace2
-    window = tk.Tk()
-    window.config(background="#CCCCCC")
-    window.geometry("{0}x{1}".format(window_width, window_height))
-
-    # Left frame
-    leftFrame = tk.Frame(window, width=200, height=600)
-    leftFrame.grid(row=0, column=0, padx=2, pady=2)
-
-    drawingSpace = tk.Canvas(leftFrame, width=800, height=600, bg='white')
-    drawingSpace.grid(row=0, column=0)
-    drawingSpace2 = drawingSpace
-
-    #tk.Label(leftFrame, text="Instructions:").grid(row=0, column=0, padx=10, pady=2)
-    #Instruct = tk.Label(leftFrame, text="1\n2\n2\n3\n4\n5\n6\n7\n8\n9\n")
-    #Instruct.grid(row=1, column=0, padx=10, pady=2)
-
-
-
-    rightFrame = tk.Frame(window, width=200, height=600)
-    rightFrame.grid(row=0, column=1, padx=2, pady=2)
-
-    tk.Label(rightFrame, text="Instructions:")\
-        .grid(row=0, column=0, padx=10, pady=2)
-
-    Instruct = tk.Label(rightFrame, text="1\n2\n2\n3\n4\n5\n6\n7\n8\n9\n")
-    Instruct.grid(row=1, column=0, padx=10, pady=2)
-
-
-    go = tk.Button(rightFrame, text='Add Square', command=add_label)
-    go.grid(row=0, column=1)
-
-    add_label(drawingSpace, "red")
-    add_label(drawingSpace, "purple")
-
-
-    return window
-
-
-def add_label(window=drawingSpace2, color="grey", width=10, height=5):
+def add_label(window=drawingCanvas, color="grey", width=10, height=5):
     label = tk.Label(window, bg=color, width=width, height=height)
     label.place(x=random.randint(0, 600), y=random.randint(0, 400))
 
@@ -71,23 +31,65 @@ def add_label(window=drawingSpace2, color="grey", width=10, height=5):
     return label
 
 
+
+
+
+
+class MainWindow(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.config(background="#CCCCCC")
+        self.geometry("{0}x{1}".format(window_width, window_height))
+
+        self.leftFrame = OwnFrame(self, True)
+        self.rightFrame = OwnFrame(self, False)
+
+        self.mainloop()
+
+
+
+class OwnFrame(tk.Frame):
+    def __init__(self, parent, left):
+        super().__init__(parent, width=200, height=600)
+        self.left = left
+        self.parent = parent
+        self.canvas = None
+
+        # Code to run if Frame is on the left side ...
+        if left:
+            self.grid(row=0, column=0, padx=2, pady=2)
+
+            self.canvas = OwnCanvas(self)
+            # TODO -1-
+            global drawingCanvas
+            drawingCanvas = self.canvas
+
+        # here on the right side
+        else:
+            self.grid(row=0, column=1, padx=2, pady=2)
+
+            tk.Label(self, text="Instructions:") \
+                .grid(row=0, column=0, padx=10, pady=2)
+
+            Instruct = tk.Label(self, text="1\n2\n2\n3\n4\n5\n6\n7\n8\n9\n")
+            Instruct.grid(row=1, column=0, padx=10, pady=2)
+
+            go = tk.Button(self, text='Add Square', command=add_label)
+            go.grid(row=0, column=1)
+
+
+class OwnCanvas(tk.Canvas):
+    def __init__(self, parent):
+        super().__init__(parent, width=800, height=600, bg='white')
+        self.grid(row=0, column=0)
+
+        self.parent = parent
+        self.labels = []
+
+
+
 def main():
-
-    labels = []
-
-    window = init_window()
-
-
-
-    #label = tkinter.Label(window, bg="red", width=10, height=5)
-    #label.place(x=0, y=0)
-
-    #label2 = tkinter.Label(window, bg="blue", width=10, height=5)
-    #label2.place(x=100, y=100)
-
-
-    window.mainloop()
-
+    MainWindow()
 
 
 if __name__ == "__main__":
